c78f16e0f7cec2cf29ce2f9be499e24d4d0b4498
Chris Hoffman <choffman@quixeyy.com>
2016-04-21 20:54:48 -0700

    update release version

--- redistrib/__init__.py
+++ redistrib/__init__.py
@@ -1,2 +1,2 @@
 __version__ = '0.5.0'
-REPO = 'https://github.com/HunanTV/redis-trib.py'
+REPO = 'https://github.com/quixey/redis-trib.py'
--- setup.py
+++ setup.py
@@ -4,17 +4,20 @@ import redistrib
 setup(
     name='redis-trib',
     version=redistrib.__version__,
-    author='Neuron Teckid',
-    author_email='lene13@gmail.com',
+    author='Chris Hoffman',
+    author_email='chris@quixey.com',
     license='MIT',
     keywords='Redis Cluster',
     url=redistrib.REPO,
     description='Redis Cluster tools in Python2',
     packages=['redistrib'],
-    long_description='Visit ' + redistrib.REPO + ' for details please.',
+    long_description='Visit ' + redistrib.REPO + ' for details. Module forked from '
+                                                 'https://github.com/HunanTV/redis-trib.py',
     install_requires=[
         'hiredis',
         'retrying',
+        'six',
+        'redis'
     ],
     zip_safe=False,
     entry_points=dict(
