68807cbb3d3d6ad44d7a679679f3652270f77e94
Chris Hoffman <choffman@quixeyy.com>
2016-05-26 15:27:37 -0700

    update test script to match redis-cluster-docker

--- test/mach_redis_instances.sh
+++ test/mach_redis_instances.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-docker run -d --name redis-cluster-a --net=host orca.quixey.com/redis-cluster-a
-docker run -d --name redis-cluster-b --net=host orca.quixey.com/redis-cluster-b
-docker run -d --name redis-cluster-c --net=host orca.quixey.com/redis-cluster-c
-docker run -d --name redis-cluster-d --net=host orca.quixey.com/redis-cluster-d
+
+IP=$(docker-machine ip default || boot2docker ip || /sbin/ifconfig docker0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}' )
+
+docker run -d -e "HOST_IP=${IP}" --name redis-cluster-a --net=host orca.quixey.com/redis-cluster-a
+docker run -d -e "HOST_IP=${IP}" --name redis-cluster-b --net=host orca.quixey.com/redis-cluster-b
+docker run -d -e "HOST_IP=${IP}" --name redis-cluster-c --net=host orca.quixey.com/redis-cluster-c
+docker run -d -e "HOST_IP=${IP}" --name redis-cluster-d --net=host orca.quixey.com/redis-cluster-d
